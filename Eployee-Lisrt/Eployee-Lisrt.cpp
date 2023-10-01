68fa0c03943f168f3398262edfe7b6f708f26f14
Matt T <mrsymtec@gmail.com>
2023-10-02 09:32:28 +1300

    Created class for the employee names and ages

--- Eployee-Lisrt/Eployee-Lisrt.cpp
+++ Eployee-Lisrt/Eployee-Lisrt.cpp
@@ -2,9 +2,45 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+class employee {
+
+public:
+    string name;
+    int age;
+    void getdata(void)
+    {
+
+        cout << "\nEnter employee name:\n";
+        getline(cin, name); //reades a line of string, allowing for white space
+        cout << "\nEnter employee age:\n";
+        cin >> age;
+        cin.ignore(); //clears buffer
+    }
+    void putdata()
+    {
+
+        cout << "\nEmployee name is: " << name;
+        cout << ":\nEmployee age is: " << age;
+
+    }
+};
 
 int main()
 {
-    
+    int i;
+
+    employee e[5];
+    for (i = 0; i < 5; i++)
+    {
+        e[i].getdata();
+    }
+    for (i = 0; i < 5; i++)
+    {
+        e[i].putdata();
+    }
+    return 0;
 }
 
